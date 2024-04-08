ac1a06c9b414b4b9f2854779d914f299e5ed5547
John Kleinad <juan_mejia13@outlook.com>
2024-04-08 14:25:30 -0600

    add links and a

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from 'react';
 import './App.css'
+import { Link } from 'react-router-dom';
 
 function App() {
 
   return <>
     aqui no hay nada
-    <a href="/addPlayers">add players</a>
+    <a href="/addPlayers">add players con a</a>
+    <Link to={'/addPlayers'}>
+      Add players con linl
+    </Link>
   </>
 }
 
